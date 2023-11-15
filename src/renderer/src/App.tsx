2e79d526ac7b07930b9829f63400f4141c4b57ea
Le Anh Tien <tienla5>
2023-11-16 02:08:35 +0700

    chore: confg router

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,7 +1,14 @@
 import HomeScreen from './screens/Home'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 const App = () => {
-  return <HomeScreen />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={'/'} element={<HomeScreen />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
