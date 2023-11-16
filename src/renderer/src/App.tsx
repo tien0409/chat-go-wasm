0279b96bc67ce1eb80b8dfb4046f8e18fc8e4af5
Le Anh Tien <latien@vcbs.com.vn>
2023-11-16 08:39:31 +0700

    feat(ui): finish menu action

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,11 +1,16 @@
 import HomeScreen from './screens/Home'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { HOME_PAGE, SIGN_IN_PAGE, SIGN_UP_PAGE } from './configs/routes'
+import SignInScreen from './screens/SignIn'
+import SignUpScreen from './screens/SignUp'
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path={'/'} element={<HomeScreen />} />
+        <Route path={HOME_PAGE} element={<HomeScreen />} />
+        <Route path={SIGN_IN_PAGE} element={<SignInScreen />} />
+        <Route path={SIGN_UP_PAGE} element={<SignUpScreen />} />
       </Routes>
     </BrowserRouter>
   )
--- src/renderer/src/components/MenuAction.tsx
+++ src/renderer/src/components/MenuAction.tsx
@@ -1,4 +1,7 @@
 import clsx from 'clsx'
+import { LogOut } from 'lucide-react'
+import { useNavigate } from 'react-router-dom'
+import { SIGN_IN_PAGE } from '../configs/routes'
 
 type MenuActionProps = {
   isMenuOpen: boolean
@@ -8,22 +11,49 @@ type MenuActionProps = {
 const MenuAction = (props: MenuActionProps) => {
   const { isMenuOpen, setIsMenuOpen } = props
 
+  const navigate = useNavigate()
+
+  const handleLogout = () => {
+    navigate(SIGN_IN_PAGE)
+  }
+
   return (
     <div
       className={clsx(
-        'fixed duration-300 inset-0 bg-black/20 z-20',
+        'fixed duration-300 inset-0 z-20',
         isMenuOpen ? 'visible opacity-100' : 'opacity-0 invisible'
       )}
-      onClick={() => setIsMenuOpen(false)}
     >
       <div
+        className="fixed duration-300 inset-0 bg-black/20"
+        onClick={() => setIsMenuOpen(false)}
+      ></div>
+
+      <div
         className={clsx(
-          'absolute top-0 duration-300 bottom-0 left-0 w-1/4 bg-gray-800',
+          'absolute top-0 duration-300 bottom-0 left-0 w-1/4 px-4 py-8 bg-yellow-50 shadow-xl',
           isMenuOpen ? 'translate-x-0' : '-translate-x-full'
         )}
       >
-        <ul>
-          <li>Logout</li>
+        <div className="flex gap-x-3 items-center">
+          <img
+            src="https://source.unsplash.com/RZrIJ8C0860"
+            alt="image"
+            className="w-10 h-10 rounded-full"
+          />
+          <div className="flex flex-col">
+            <h4 className="font-semibold text-sm">Username</h4>
+          </div>
+        </div>
+
+        <ul className="mt-8">
+          <li
+            className="flex p-2 gap-x-3 items-center cursor-pointer hover:bg-yellow-500/20 transition-colors"
+            onClick={handleLogout}
+          >
+            <LogOut />
+            <span>Logout</span>
+          </li>
         </ul>
       </div>
     </div>
