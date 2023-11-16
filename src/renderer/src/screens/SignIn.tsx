8ebc9e341437e4865979be2b569d872c1573d3ad
Le Anh Tien <latien@vcbs.com.vn>
2023-11-16 11:10:07 +0700

    chore: handle navigate button

--- src/renderer/src/screens/SignIn.tsx
+++ src/renderer/src/screens/SignIn.tsx
@@ -2,7 +2,7 @@ import Input from '../components/Input'
 import { FormEvent } from 'react'
 import useAuthStore from '../stores/useAuthStore'
 import { useNavigate } from 'react-router-dom'
-import { HOME_PAGE } from '../configs/routes'
+import { HOME_PAGE, SIGN_UP_PAGE } from '../configs/routes'
 
 const SignInScreen = () => {
   const navigate = useNavigate()
@@ -19,7 +19,12 @@ const SignInScreen = () => {
     <div className="fixed top-20 left-1/2 min-w-[500px] -translate-x-1/2">
       <div className="text-right">
         <span className="mr-4">Bạn chưa có tài khoản?</span>
-        <button className="border py-2 px-5 rounded border-gray-200">Đăng ký</button>
+        <button
+          className="border py-2 px-5 rounded border-gray-200"
+          onClick={() => navigate(SIGN_UP_PAGE)}
+        >
+          Đăng ký
+        </button>
       </div>
 
       <h1 className="mb-3 mt-10 text-3xl font-bold">Chào mừng!</h1>
@@ -34,7 +39,7 @@ const SignInScreen = () => {
           className="bg-prim-100 text-white font-semibold w-full rounded py-3 mt-2 hover:bg-prim-100/90 duration-200"
           type="submit"
         >
-          Sign In
+          Đăng nhập
         </button>
       </form>
     </div>
--- src/renderer/src/screens/SignUp.tsx
+++ src/renderer/src/screens/SignUp.tsx
@@ -1,17 +1,26 @@
 import Input from '../components/Input'
 import { FormEvent } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { SIGN_IN_PAGE } from '../configs/routes'
 
 const SignUpScreen = () => {
+  const navigate = useNavigate()
+
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
-    console.log('submit')
+    navigate(SIGN_IN_PAGE)
   }
 
   return (
     <div className="fixed top-20 left-1/2 min-w-[500px] -translate-x-1/2">
       <div className="text-right">
         <span className="mr-4">Bạn đã có tài khoản?</span>
-        <button className="border py-2 px-5 rounded border-gray-200">Đăng nhập</button>
+        <button
+          className="border py-2 px-5 rounded border-gray-200"
+          onClick={() => navigate(SIGN_IN_PAGE)}
+        >
+          Đăng nhập
+        </button>
       </div>
 
       <h1 className="mb-3 mt-10 text-3xl font-bold">Chào mừng!</h1>
@@ -27,7 +36,7 @@ const SignUpScreen = () => {
           className="bg-prim-100 text-white font-semibold w-full rounded py-3 mt-2 hover:bg-prim-100/90 duration-200"
           type="submit"
         >
-          Sign Up
+          Đăng ký
         </button>
       </form>
     </div>
